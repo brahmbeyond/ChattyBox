8a859fdf32df4e5e440a3e73b3d2e8323fd16e1b
autumn-absconds <shubham429.yadav@gmail.com>
2023-05-28 14:38:40 +0530

    ui+passward feature

--- src/components/EnterRoom.jsx
+++ src/components/EnterRoom.jsx
@@ -1,58 +1,84 @@
 import React, { useState } from 'react'
 import Rooms from './Rooms';
-import { useNavigate } from 'react-router-dom';
+import { db } from '../firebase-config'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 import Grid from '@mui/material/Unstable_Grid2';
-import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import InputBase from '@mui/material/InputBase';
-import Typography from '@mui/material/Typography';
 import { Button, TextField } from '@mui/material';
-
-
+import EnterRoomModal from './EnterRoomModal';
 
 
 const EnterRoom = () => {
     const [roomName, setRoomName] = useState("");
-    const navigate = useNavigate();
+    const [roomPassword, setRoomPassword] = useState("");
+    //modal states
+    const [open, setOpen] = useState(false);
+    const [create, setCreate] = useState(false);
 
-    const enterRoom = () => {
-        navigate(`./Chat/${roomName}`)
+    const handleClose = () => setOpen(false);
+
+    const collectionRef = collection(db, "messages");
+
+
+    //  when enter , check for the if room empty or not (by query data using condition for roomname) then check if array is empty or not then procede
+    const checkRoom = () => {
+        const q = query(collectionRef, where("roomName", "==", roomName));
+        const getRoom = async () => {
+            const querySnapshot = await getDocs(q);
+            let data = [];
+            querySnapshot.forEach((doc) => {
+                data.push({ ...doc.data(), id: doc.id });
+
+            });
+
+            if (data.length === 0) { //create new room
+                setCreate(true);
+
+            }
+            else {  // enter already present
+                setRoomPassword(data[0].roomPassword)
+                setOpen(true);
+            }
+        }
+
+        getRoom();
+
     }
+
 
     return (
         <>
+            {/* modal for verify password */}
+            {open && <EnterRoomModal roomName={roomName} heading={"Enter Room "} password={roomPassword} open={open} setOpen={setOpen} buttonName={"Verify Password"} />}
+
+            {/* modal for create room and password */}
+            {create && <EnterRoomModal roomName={roomName} heading={"Create Room "} password=" " open={create} setOpen={setCreate} buttonName={"Set Password"} />}
+
+
             <Grid container spacing={2} >
                 <Grid xs={12} sm={4} >
                     <Rooms />
                 </Grid>
 
-                <Grid xs={12} sm={8} display="flex" justifyContent="center" alignItems="center">
+                <Grid xs={12} sm={8} display="flex" justifyContent="center" alignItems="center" sx={{
+                height:'100vh',
 
+            }} >
                     <Card sx={{ display: 'flex', maxWidth: 361 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                             <CardContent sx={{ flex: '1 0 auto' }}>
-                                {/* <Typography component="div" variant="h5">
-                                Enter Room / Create Room
-                                </Typography> */}
-
-
                                 <TextField
                                     label="Enter Room / Create Room"
                                     variant="standard"
                                     color="warning"
                                     focused
-
                                     value={roomName}
-                                    //   placeholder='type room name'
                                     onChange={(e) => setRoomName(e.target.value)}
-
-
-
                                 />
-                                <Button color="warning" onClick={enterRoom} variant="contained" sx={{ marginTop: 3 }}>Enter</Button>
+                                <Button color="warning" onClick={checkRoom} variant="contained" sx={{ marginTop: 3 }}>Enter</Button>
 
                             </CardContent>
                         </Box>
@@ -62,34 +88,9 @@ const EnterRoom = () => {
                             image="https://i.ibb.co/vdW5R8n/OIG.jpg"
                         />
                     </Card>
-
-
-
-
-                    {/* <label htmlFor="EnterRoom" >   Enter Room / Create Room
-                    </label>
-                    <input type="text"
-                        name="room"
-
-                        value={roomName}
-                        placeholder='type room name'
-                        onChange={(e) => setRoomName(e.target.value)}
-                    />
-                    <button onClick={enterRoom} >Enter</button> */}
-
                 </Grid>
-
-
             </Grid>
 
-            {/* <div >
-                <div >
-                  
-                </div>
-                <div >
-                
-                </div>
-            </div> */}
         </>
 
     )
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,4 @@
 
-
-
 const Welcome = () => {
     return (
         <>
